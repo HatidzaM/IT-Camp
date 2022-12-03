58c96b117289a87b9f03ae864316513e7c78e79b
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2022-12-03 15:06:23 +0100

    edited

--- JavaScript/15.cas str/metode stringova.js
+++ JavaScript/15.cas str/metode stringova.js
@@ -17,17 +17,22 @@ console.log(rec[4]);
 //rec koja trb da se nadje pod navodnicima mora biti pod obicnim ako je string okruzen duplim ili obrnuto
 //1. " "
 const string = "danas je 'padala' kisa"
+console.log(string)
 // \ 
 //2. ' ' 
 const string2 = 'danas je \"padala\" kisa'
+console.log(string2)
+
 //3. ` `
 const string3 = `danas je padala kisa`
+console.log(string3)
+
 
 //primena  \(backslash) na kraju reda:
 //sa \ ne mozemo nastaviti liniju koda, vec samo string
 const string5 = "hduhduidsudugdififgfiysgyfisgfyis. \
-kjkahkajdhjkhdkdah"
-const string4 = "hduhduidsudugdififgfiysgyfisgfyis. \nkjkahkajdhjkh\ndkdah"
+kjkahkajdhjkhdkdah" //stavljamo novi red samo za nas u kodu
+const string4 = "hduhduidsudugdififgfiysgyfisgfyis. \nkjkahkajdhjkh\ndkdah" //ispisace se u konzoli
 console.log(string4)
 
 //postoje 3 metode za ekstraktovanja (uzimanje dela stringa):
@@ -40,3 +45,9 @@ console.log(string6.slice(0,4))
 console.log(string6.slice(-6, -1))
 console.log(string6.substring(0,9))
 console.log(string6.substr(0,8))
+
+const stringg = "hatidza"
+let ha = stringg.slice(0, 2)
+console.log(ha)
+console.log(stringg.slice(-5, -1))
+console.log(stringg.substr(2, string.length))
