91c1582a8523c69931a443768e6a624eaa0c4b8e
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2022-12-20 19:57:34 +0100

    domaci 24

--- JavaScript/24.cas/domaci.js
+++ JavaScript/24.cas/domaci.js
@@ -1,2 +1,18 @@
 //3.napraviti funkciju koja ocekuje cetvorocifren broj, a vraca broj kojem su zamenjene  prva i treca cifra i  druga i cetvrta
-//4752   5247
+//4752  (0123) | 5247  (2301)
+
+function cetvorocifren(broj){
+    const duzina = broj.toString().length;
+    const stringbr = broj.toString();
+    if(duzina !== 4 || isNaN(stringbr)){
+        return `broj mora imati 4 cifre`
+    }
+    else{
+        const prvideo = stringbr.slice(0,2);
+        const drugideo = stringbr.slice(2);
+        const novibr = drugideo.concat(prvideo);
+        return +novibr
+    }
+}
+console.log(cetvorocifren(1234));
+console.log(cetvorocifren(4752));
