77cd34cbca62388f4e350a8d3ed3cfa124106b85
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2022-12-29 21:00:27 +0100

    a

---
+++ JavaScript/29.cas/app.js
@@ -0,0 +1,42 @@
+// metode za prikazivanje datuma
+
+const trenutno = new Date();
+console.log(trenutno);
+
+//1. toString()
+console.log(trenutno.toString());
+
+//2. toUTCString()
+console.log(trenutno.toUTCString());
+
+//2. toDateString()
+console.log(trenutno.toDateString());
+
+//4. toISOString()
+console.log(trenutno.toISOString());
+
+//5. toTimeString()
+console.log(trenutno.toTimeString())
+
+//6. toLocaleTimeString()
+console.log(trenutno.toLocaleTimeString());
+
+//Get metode:
+//za dobijanje informacija iz Date objekta
+//getFullYear()
+//getMonth()
+console.log(trenutno.getMonth()+1);
+//getDate()
+//getDay() 0-6 ned-sub
+let nedelja = new Date("2022 Dec 25");
+console.log(nedelja.getDay());
+//getHours()
+//getMinutes()
+//getSeconds()
+//getMiliseconds()
+//getTime()
+
+
+//Set metode:
+//dozvoljavaju menjanje nekih vrednosti koda Date objekta
+trenutno.setDate(19);
