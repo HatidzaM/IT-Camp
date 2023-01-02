8c60cda22b4dfbd3102c20a8eaebad6034c9e954
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-02 22:37:15 +0100

    30

---
+++ JavaScript/30.cas nizovi/domaci.js
@@ -0,0 +1,3 @@
+// Iz datog niza ispitati koliko elemenata je tipa string, number, boolean, koliko elemenata je zapravo niz?
+const nizz = [ "jagoda", 25, true, "mandarina", [1, 2, 3], "ananas", 49, 694, "tresnja" ];
+
