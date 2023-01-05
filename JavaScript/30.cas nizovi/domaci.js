22a94d3af2d47ffc8f1393d5f3755397f3407ab8
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-05 23:39:10 +0100

    domaci

--- JavaScript/30.cas nizovi/domaci.js
+++ JavaScript/30.cas nizovi/domaci.js
@@ -35,25 +35,30 @@ function types(niz) {
     let numberTipa = 0;
     let booleanTipa = 0;
     let nizTipa = 0;
-    for (item of niz) {
-      if (typeof item === "string") {
+    for (item of niz){
+        if (typeof item === "string"){
         stringTipa++;
-      } else if (typeof item === "number") {
+        }
+        else if (typeof item === "number"){
         numberTipa++;
-      } else if (typeof item === "boolean") {
+        }
+        else if (typeof item === "boolean"){
         booleanTipa++;
-      } else if (item instanceof Array) {
+        }
+        else if (item instanceof Array){
         nizTipa++;
-      }
+        }
     }
     return `U datom nizu su se nasla: \n ${stringTipa} stringova,
-     \n ${numberTipa} brojeva, \n ${booleanTipa} booleana,
-     \n ${nizTipa} nizova.`;
-  }
-  
-  console.log(types(nizz));
-  
-  // Novi zapis for petlje, koji se odnosi na sve iterirajuce objekte.
-  // for (let i of nizz) {
-  //   console.log(i);
-  // }
+    \n ${numberTipa} brojeva, \n ${booleanTipa} booleana,
+    \n ${nizTipa} nizova.`;
+}
+
+console.log(types(nizz));
+
+
+
+// Novi zapis for petlje, koji se odnosi na sve iterirajuce objekte.
+// for (let i of nizz) {
+//   console.log(i);
+// }
