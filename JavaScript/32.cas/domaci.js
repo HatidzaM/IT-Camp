6d726eb3fe427629fb5cd7a98b33c5a3aeda4ffc
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-09 22:47:25 +0100

    domaci

---
+++ JavaScript/32.cas/domaci.js
@@ -0,0 +1,6 @@
+// Za dati niz izvrsiti:
+// Sortiranje niza od najveceg ka najmanjem broju,
+// Sortiranje niza od najmanjeg ka najvecem broju,
+// Napraviti funkciju koja vraca najveci element niza,
+// Napraviti funkciju koja vraca najmanji element niza (na 2 nacina).
+// const niz = [-5, 6, 10, 45, 99, -44];
