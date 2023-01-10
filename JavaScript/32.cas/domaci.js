ed7bf4004a3392cf9fba7d91f3e4d16e2a2fcf31
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-10 19:59:37 +0100

    domaci32

--- JavaScript/32.cas/domaci.js
+++ JavaScript/32.cas/domaci.js
@@ -3,19 +3,31 @@
 // Sortiranje niza od najmanjeg ka najvecem broju,
 // Napraviti funkciju koja vraca najveci element niza,
 // Napraviti funkciju koja vraca najmanji element niza (na 2 nacina).
-const niz = [-5, 6, 10, 45, 99, -44];
+const brojevi = [-5, 6, 10, 45, 99, -44];
 
 //od najveceg
-// niz.sort((a,b) => b-a);
-// console.log(niz);
+brojevi.sort((a,b) => b-a);
+console.log(brojevi);
 
 
 //od najmanjeg 
-// niz.sort((a,b) => a-b);
-// console.log(niz);
+brojevi.sort((a,b) => a-b);
+console.log(brojevi);
 
 
 // vraca najveci
+
+//I nacin
+function maxel(niz){
+    niz.sort((a,b)=> b-a);
+    return niz[0]
+}
+console.log(maxel(brojevi));
+
+//II nacin
+console.log(Math.max.apply(null, brojevi));
+
+//III nacin /moj
 function najveci(niz){
     // const veciBr = a > b ? a : b;
     //     return veciBr
@@ -28,4 +40,34 @@ function najveci(niz){
     }
     return max
 }
-console.log(najveci([-5, 6, 10, 45, 99, -44]))
+console.log("ja"+najveci(brojevi));
+
+//IV nacin
+function maxx(niz){
+    let max = Number.NEGATIVE_INFINITY;
+    for(let broj of niz){
+        if(broj>max){
+            max=broj
+        }
+    }
+    return max
+}
+console.log(maxx(brojevi));
+
+
+
+// vraca najmanji
+function najmanji(niz){
+    // const veciBr = a > b ? a : b;
+    //     return veciBr
+    let min = niz[0];
+    for(let i=1; i<niz.length; i++){
+        if(niz[i] < min){
+            min = niz[i]
+        }
+        // else{ continue }
+    }
+    return min
+}
+console.log(najmanji([-5, 6, 10, 45, 99, -44]));
+
