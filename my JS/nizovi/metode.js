d7466fd84e8b73d690b488a8bac188a0ffd4474c
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-12 16:59:38 +0100

    metode nizova

---
+++ my JS/nizovi/metode.js
@@ -0,0 +1,12 @@
+//to string()  /  join()
+//push()  = dodaje na kraju
+//pop()  = vraca zadnji izbrisani
+//unshift()  = dodaje na pocetku
+//shift()  = vraca prvi izbrisani
+//delete  = pravi se undefined
+//concat()  = spajanje
+//sort()
+//reverse()
+//splice( -, -, -, ...)
+//slice()  = vraca deo niza
+
