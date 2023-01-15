e66927f7c20052c2723e46a3183c4eedb0b00cdd
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-15 23:33:06 +0100

    domaci

---
+++ JavaScript/35.cas/domaci.js
@@ -0,0 +1,10 @@
+// 1. Pomocu for petlje napraviti novi niz, koji ce sadrzati samo godine vece od 17.
+
+// 2.
+// Prvi deo. Napraviti pomocni niz koji ce sadrzati elemente od oba niza.
+// Drugi deo. Napraviti jos jedan pomocni niz koji ce iz prvog dodatnog niza uzeti parne brojeve.
+// Treci deo. U drugom pomocnom nizu umetnuti na prva dva mesta 10 i 20.
+// Funkcija na kraju treba da vrati dva napravljena pomocna niza.
+
+const DATA1 = [2, 26, 38, 75, 11, 29];
+const DATA2 = ["a", "b", "c", "d", "e"];
