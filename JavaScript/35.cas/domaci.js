678df3f5ad5acda933fec58cc22d6e96fdd52c76
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-17 23:08:01 +0100

    ez

--- JavaScript/35.cas/domaci.js
+++ JavaScript/35.cas/domaci.js
@@ -1,4 +1,12 @@
 // 1. Pomocu for petlje napraviti novi niz, koji ce sadrzati samo godine vece od 17.
+const niz = [13,4,33,6,17,18,22,25];
+const novi = [];
+for(let i of niz){
+    if(i>17){
+        novi.push(i)
+    }
+}
+console.log(novi);
 
 // 2.
 // Prvi deo. Napraviti pomocni niz koji ce sadrzati elemente od oba niza.
@@ -8,3 +16,16 @@
 
 const DATA1 = [2, 26, 38, 75, 11, 29];
 const DATA2 = ["a", "b", "c", "d", "e"];
+
+const task = (arr1, arr2) => {
+const pomocni = arr1.concat(arr2);
+const pomocni2 = [];
+for (let br of pomocni) {
+    if (br % 2 === 0) {
+        pomocni2.push(br);
+    }
+}
+pomocni2.unshift(10, 20);
+return [pomocni, pomocni2];
+};
+console.log(task(DATA1, DATA2));
