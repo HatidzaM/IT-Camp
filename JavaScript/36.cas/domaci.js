621d75e2a0f893e34e20444bb9fd650ae424d77f
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-19 20:09:01 +0100

    domaci

---
+++ JavaScript/36.cas/domaci.js
@@ -0,0 +1,43 @@
+// 1. Iz niza od 10 elemenata izracunati sumu parnih brojeva niza.
+
+
+// 2. From the array of numbers, choose even double even numbers and compute the sum using Array's filter, map and reduce methods.
+
+
+// 3. Write a JavaScript program to find the most frequent item of an array.
+// Sample array :
+const arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+
+let pojavljivanje = 1;
+var element;
+let brojac = 0;
+for(let i=0; i<arr1.length; i++){
+    for(let k=i; k<arr1.length; k++){
+        if(arr1[i]===arr1[k]){
+            brojac++;
+            if(brojac>pojavljivanje){
+                element = arr1[i];
+                pojavljivanje = brojac;
+            }
+        }
+        
+    }
+    brojac = 0
+}
+console.log(`to je element ` + element + ` i pojavljuje se ` + pojavljivanje + ` puta`);
+
+// 4. Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+
+const change = (str)=>{
+    let newStr = "";
+    for(i=0; i<str.length; i++){
+        if(str[i]===str[i].toLowerCase()){
+            newStr+=str[i].toUpperCase()
+        }
+        else{
+            newStr+=str[i].toLowerCase()
+        }
+    }
+    return newStr
+}
+console.log(change(`The Quick Brown Frog`));
