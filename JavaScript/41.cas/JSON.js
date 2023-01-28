7869cfe9b8805f51eb3d88b358ea5016d28f0096
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-28 18:02:57 +0100

    JSON

---
+++ JavaScript/41.cas/JSON.js
@@ -0,0 +1,33 @@
+//  JSON - javaScript object Notation  //
+
+// JSON je format za skladistenje i prenos podataka
+//najcesce se koristi kada se podaci salju sa servera ka WEB stranici
+
+//JSON podaci su vrslu slicni js objektu i oni se zapisuju :
+// const person = {
+//     "firstName": "amer",
+//     "lastName": "honic",
+//     "age":19,
+// }
+
+//keys/names zahtevaju duple navodnika, za razliku od js objekta
+
+
+const person = {
+    firstName: "amer",
+    lastName: "honic",
+    age:19,
+}
+
+//dve metode za pretvaranje
+
+//1. JSON.stringify()  = pretvara nas objekat u JSON format pa JSON u string
+const strObject = JSON.stringify(person);
+console.log(strObject);
+console.log(typeof strObject);
+
+//2. JSON.parse() = analizira se neki string i u slucaju mogucnosti pretvara u objekaat(nama citljiv podatak)
+const obj = JSON.parse(strObject);
+console.log(obj);
+console.log(typeof obj);
+
