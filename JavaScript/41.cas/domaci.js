e8c586ec25f892801e852f5df282cb706153cf14
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-01-31 01:26:03 +0100

    domaci

--- JavaScript/41.cas/domaci.js
+++ JavaScript/41.cas/domaci.js
@@ -1,3 +1,4 @@
+//1. Zadatak
 // Napraviti objekat sa sledecim svojstvima:
 // 1. firstName,
 // 2. lastName,
@@ -10,51 +11,66 @@ const objekat = {
     firstName:"hatidza",
     lastName:"mahmutovic",
     language:"turski",
+    setLanguage:function(){
+        prompt("set language:");
+        return prompt
+    },
+    setNickName: function(){
+        let prvo = "";
+        let drugo = "";
+        let sastavljanje="";
+        for(let i = 0; i<2; i++){
+            prvo += this.firstName[i]
+            drugo += this.lastName[i]
+        }
+        sastavljanje = prvo + drugo
+        return sastavljanje
+    }
 }
+console.log(objekat.setNickName());
 
 
 
-
-
-// 2. Zadatak
-//  const radnik = {
-//     firstName: "Dzenan",
-//      lastName : "Kosuta",
-//      fullname:function(){
-//      },
-//      adresa:{
-//          ulica:"avnoja",
-//          broj:"bb",
-//          grad:"Novi Pazar",
-//          getAdress:function(){
+//2. Zadatak
+const radnik = {
+    firstName: "Dzenan",
+    lastName : "Kosuta",
+    fullname:function(){
+        return this.firstName + this.lastName
+    },
+    adresa:{
+        ulica:"avnoja",
+        broj:"bb",
+        grad:"Novi Pazar",
+        getAdress:function(){
+            // return `${fullname()} zivi u ulici ${radnik.adresa}`
+            return radnik.firstName + radnik.lastName + `zivi u ulici` + this.ulica + this.broj + this.grad 
 // vraca Dzenan Kosuta zivi u ulici Avnoja u Novom Pazaru.
-//          }
-//      }
+        }
+    }
+}
+console.log(radnik.fullname());
+console.log(radnik.adresa.getAdress());
 
 
 
+//3. Zadatak
+const automobil = {
+    marka: "Audi",
+    model: "Q7",
+    boja: "Bela",
+    pogon: "quattro",
+    menjac: "Automatik",
+    km:240000,
+    vlasnik:[062321552,063930630],
+    garaza:{
+        parking:"JKP Servis",
+        vikend:"od 17 free",
+        satnaKarta: "50",
+        dnevnaKarta: "200",
+        mesecnaKarta: "2000",
+        platiZa: function(od, do) {
 
-
-
-
-
-// 3. Zadatak
-// const automobil = {
-//     marka: "Audi",
-//     model: "Q7",
-//     boja: "Bela",
-//     pogon: "quattro",
-//     menjac: "Automatik",
-//     km:240000,
-//     vlasnik:[062321552,063930630],
-//     garaza:{
-//         parking:"JKP Servis",
-//         vikend:"od 17 free",
-//         satnaKarta: "50",
-//         dnevnaKarta: "200",
-//         mesecnaKarta: "2000",
-//         platiZa: function(od, do) {
-//
-// }
-//     }
-// }
+}
+    }
+}
