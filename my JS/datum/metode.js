0755195f420bb9fd4612574f5263a3264b1f7842
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-02-14 01:22:36 +0100

    nije mmi bitan datum

---
+++ my JS/datum/metode.js
@@ -0,0 +1,34 @@
+//default: 1.1.1970.
+
+//1. new Date()
+//2. new Date(year, month, day, hour, minutes, seconds, miliseconds)
+//3. date(x)       dodat broj milisekundi na 1.1.1970.
+//4. Date("...")   string se parsenuje
+
+//1. toString()              evropsko
+//2. toUTCString()           evropsko -1h
+//2. toDateString()          datum
+//4. toISOString()           isto kao new Date()
+//5. toTimeString()          vreme
+//6. toLocaleTimeString()    lokalno
+
+//Get metode:  //za dobijanje informacija iz Date objekta
+//getFullYear()
+//getMonth()     0-11
+//getDate()
+//getDay()       0-6 ned-sub
+//getHours()
+//getMinutes()
+//getSeconds()
+//getMiliseconds()
+//getTime()
+
+//Set metode:  //dozvoljavaju menjanje nekih vrednosti koda Date objekta
+// setFullYear()
+// setMonth()
+// setDate()
+// setHours()
+// setMinutes()
+// setSeconds()
+// setMiliseconds()
+// setTime()
