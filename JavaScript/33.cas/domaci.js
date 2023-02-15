96bf8af3537a1d9aa9ab2c4dc0a498d087197db7
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-02-15 15:07:51 +0100

    coz

--- JavaScript/33.cas/domaci.js
+++ JavaScript/33.cas/domaci.js
@@ -8,19 +8,19 @@ console.log(nizS);
 
 // 2. Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. For example if you accept 025468 the output should be 0-254-6-8
 
-const addsum = (num) => {
-    let newNum = "";
-    for(let i = 0; i<= num.length-1; i++){
-        if(num[i] % 2 === 0 && num[i+1]%2===0){
-            newNum += num[i] + "-"
-        }
-        else{
-            newNum += num[i]
-        }
-    }
-    return newNum
-}
-console.log(addsum("012345"));
+// const addsum = (num) => {
+//     let newNum = "";
+//     for(let i = 0; i<= num.length-1; i++){
+//         if(num[i] % 2 === 0 && num[i+1]%2===0){
+//             newNum += num[i] + "-"
+//         }
+//         else{
+//             newNum += num[i]
+//         }
+//     }
+//     return newNum
+// }
+// console.log(addsum("012345"));
 
 const addSomething = (num) => {
     let newNum = "";
