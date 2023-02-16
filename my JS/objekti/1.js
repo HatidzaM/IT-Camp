e4bcea399cb05a70f5079a474c7e5e91870acb33
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-02-16 12:21:05 +0100

    json

---
+++ my JS/objekti/1.js
@@ -0,0 +1,17 @@
+//pristupanje odredjenim vrednostima objekta mozemo izvrsiti na 2 nacina
+//jedan key:value par  zove se  PROPERTY (svojstvo)
+
+//1. objectName.ptopertyName(person.firstName)
+//2. objectName["propertyName"]
+// (person["firstName"])
+
+
+
+
+
+//  THIS  je keyword koja ukazuje na neki objekat
+//nije promenljiva i ne mozemo promeniti vrednost this
+
+//this ukazuje na razlicit objekat u zavisnosti od toga gde se koristi
+//u nekom objektu this ukazuje na taj objekat
+//u global scope this ukazuje na global object
