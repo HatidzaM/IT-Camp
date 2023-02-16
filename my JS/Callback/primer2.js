ac634884ae4d4427555f6e1290f33ad441a75711
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-02-16 02:30:46 +0100

    mhmm

--- my JS/Callback/primer2.js
+++ my JS/Callback/primer2.js
@@ -1,9 +1,9 @@
 function glavnaFunkcija (sporedna){
     console.log("ovo je glavna funkcija");
-    sporedna(3,4,5);
+    sporedna(3,4,5);  //pozivamo je u glavnoj
 }
 
-function aritmeticka(a,b,c){
+function aritmeticka(a,b,c){  //definisemo je
     console.log(+((a+b+c)/3).toFixed(2))
 }
-glavnaFunkcija(aritmeticka);
+glavnaFunkcija(aritmeticka);  //lociramo je
---
+++ my JS/closure funkcija/1.js
@@ -0,0 +1,9 @@
+//CLOSURE FUNKCIJA//
+
+//poznata je cinjenica da kada se izvrsi funkcija , sve njene lokalne pomenljive pokupi garbage colector i one prestaju da postoje u memoriji
+//medjutim, to nije slucaj za funkciju cija promenj=ljjiva zahteva neja druga funkcija
+
+// closure je funkcija koja 
+//            !!!!ima pravo pristupa promenljivim iz domena druge funkcije!!!!
+//to se najcesce postize uredjivanjem funkcije unutar druge funkcije nakon cega se postize tzv
+//                                       BLOCK CHAIN
