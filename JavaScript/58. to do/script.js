5ac6be53d060beb6f49186b4c4c69d1c828a46b0
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-03-24 23:46:02 +0100

    to do

--- JavaScript/58. to do/script.js
+++ JavaScript/58. to do/script.js
@@ -34,3 +34,31 @@ console.log(select);
 const options = select.children;
 console.log(options);
 Array.from(options).forEach((option) => console.log(option.value));
+
+var buttonElement = document.querySelector(".options");
+
+buttonElement.addEventListener("click", function () {
+    var selectElement = document.querySelector("select.options");
+    var selectedValue = selectElement.value;
+    console.log("Selektovana vrijednost je: " + selectedValue);
+
+    console.log(container.children);
+    const allItems = Array.from(container.children);
+    if (selectedValue === "completed") {
+        allItems.map((item) => (item.style.display = ""));
+        const uncompleted = allItems.filter((item) => item.style.textDecoration !== "line-through");
+        uncompleted.forEach((item) => (item.style.display = "none"));
+    }
+    else if (selectedValue === "uncompleted") {
+        allItems.map((item) => (item.style.display = ""));
+        const completed = allItems.filter((item) => item.style.textDecoration === "line-through");
+        completed.forEach((item) => (item.style.display = "none"));
+    } 
+    else {
+    allItems.map((item) => (item.style.display = ""));
+    }
+});
+
+// Random broj izmedju 1 i 20:
+// const randomBr = Math.floor(Math.random() * 20) + 1;
+// console.log(randomBr);
