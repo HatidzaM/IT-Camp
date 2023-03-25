5ddc312a757bda8c785530f28ea7edcccc8ec050
hatidzamahmutovic <hatidzamahmutovic05@gmail.com>
2023-03-25 01:56:52 +0100

    cepanje

---
+++ JavaScript/guess the number/app.js
@@ -0,0 +1,10 @@
+let button = document.getElementsByClassName("button");
+let input = document.getElementById("inputBr");
+
+button.addEventListener("click", function () {
+    const randomBr = Math.floor(Math.random() * 20) + 1;
+    console.log(randomBr);
+    // let pokusaji = input.value - 1; 
+
+    
+});
